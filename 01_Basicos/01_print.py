1b8dd7a0fc7eca444b9a67ae046e1f82c7e5be04
Quioba <pukemeierd@gmail.com>
2025-09-12 09:07:02 -0300

    Ejercicios básicos con print

---
+++ 01_Basicos/01_print.py
@@ -0,0 +1,5 @@
+print("Hello, World!") # Imprime un mensaje en la consola
+print("Estoy aprendiendo Python") # Otro mensaje
+print("68+1") # Imprime la cadena "68+1"
+print(68+1) # Imprime el resultado de la suma 68 + 1
+print("El resultado de 68 + 1 es:", 68 + 1) # Imprime un mensaje junto con el resultado de la suma
