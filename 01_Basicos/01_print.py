bcb0b5773081d773cdc2106d643ee84d35502a64
Quioba <pukemeierd@gmail.com>
2025-09-12 10:00:06 -0300

    Más comandos con print y asignación de valores

--- 01_Basicos/01_print.py
+++ 01_Basicos/01_print.py
@@ -1,5 +1,13 @@
 print("Hello, World!") # Imprime un mensaje en la consola
 print("Estoy aprendiendo Python") # Otro mensaje
 print("68+1") # Imprime la cadena "68+1"
-print(68+1) # Imprime el resultado de la suma 68 + 1
-print("El resultado de 68 + 1 es:", 68 + 1) # Imprime un mensaje junto con el resultado de la suma
+print(68+1) # Imprime el resultado de la suma 68 + 1 
+a = 8
+b = 2
+c = 4
+print(a, b, c) # Imprime los valores de a, b y c
+print(a - b + c) # Imprime el resultado de la operación a - b + c
+print(a * b / c) # Imprime el resultado de la operación a * b / c
+print(a ** c) # Imprime el resultado de a elevado a la potencia c
+print(a % b == a // b) # Imprime el resto de la división de a entre b, comparado con la división entera de a entre b
+print (a > b and c > a) # Imprime el resultado de la comparación lógica entre a, b y c
