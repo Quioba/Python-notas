b87b56d00f2878ba8f4680916ab30ae691163639
Quioba <pukemeierd@gmail.com>
2025-09-24 20:17:48 -0300

    cambios implementados

--- 01_Basicos/01_print.py
+++ 01_Basicos/01_print.py
@@ -11,3 +11,17 @@ print(a * b / c) # Imprime el resultado de la operación a * b / c
 print(a ** c) # Imprime el resultado de a elevado a la potencia c
 print(a % b == a // b) # Imprime el resto de la división de a entre b, comparado con la división entera de a entre b
 print (a > b and c > a) # Imprime el resultado de la comparación lógica entre a, b y c
+print("sape")
+print("Mucho tiempo dando vueltas a la ruleta, mirando los kilómetros")
+x = "10" 
+y = 5
+print(x * y) # Imprime la cadena x repetida y veces
+print(int(x) * y) # Convierte x a entero y luego lo multiplica por y
+print("a != b?", a != b) # Imprime si a es diferente de b
+nombre = "Celi"
+print("probando probando, esta cosa funciona siquiera?")
+print(f"Hola {nombre}, cómo estás bola?") #esto no aparece cuando se ejecuta
+d = "Darío" # o sea los # 
+print(f"{d} y {nombre} son mejores amigos") #estos mensajes verdes  
+print("Celi ayuda me convertí en un porgrama de programación :c" )
+
